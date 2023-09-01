431d2fd0e8cffcb58955ef87d0116b5419ecf66c
Neokyuubi <neokyuubi@gmail.com>
2023-09-01 04:00:53 +0100

    Emhanced pagination and added inputs

--- src/main.js
+++ src/main.js
@@ -1,56 +1,48 @@
-// Axios - Promise based HTTP client for the browser and node.js (Read more at https://axios-http.com/docs/intro).
 import axios from "axios";
-// Cheerio - The fast, flexible & elegant library for parsing and manipulating HTML and XML (Read more at https://cheerio.js.org/).
 import * as cheerio from "cheerio";
-// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/).
 import { Actor, log } from "apify";
 
-// The init() call configures the Actor for its environment. It's recommended to start every Actor with an init().
 await Actor.init();
 
-// Structure of input is defined in input_schema.json
 const input = await Actor.getInput();
-const { url, pageLimit } = input;
+const { url, domain, mainCssSelector, nextPageSelector, nameSelector, annotationSelector, contentSelector, linkSelector, imageSelector } = input;
 const params = {
     headers: {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
 };
 
-let page = 1;
-let dynamicUrl = `${url}?p.Page=${page}`;
+let dynamicUrl = url;
 let hasNextPage = true;
 const clients = [];
 
-while (hasNextPage && page <= pageLimit) 
+while (hasNextPage && dynamicUrl)
 {
-    // Fetch the HTML content of the page.
-    const response = await axios.get(`${url}?p.Page=${page}`, params);
+    const response = await axios.get(dynamicUrl, params);
 
-    // Parse the downloaded HTML with Cheerio to enable data extraction.
     const $ = cheerio.load(response.data);
 
-    // Extract all clients from the page (tag name and text).
-    $(".list-item-wrapper").each((i, element) => 
+    $(mainCssSelector).each((i, element) => 
     {
         const clientObject = {
-            Name: $(element).find(".list-item-heading").html().trim(),
-            Annotation: $(element).find(".list-item-anotation").html().trim(),
-            Content: $(element).find(".overlay-txt").html().trim(),
-            Link: $(element).find("a").attr('href'),
-            Image: $(element).find(".list-item-image.js-lazy-loading-bg-img").attr('data-src')
+            Name: $(element).find(nameSelector).html().trim(),
+            Annotation: $(element).find(annotationSelector).html().trim(),
+            Content: $(element).find(contentSelector).html().trim(),
+            Link: $(element).find(linkSelector).attr('href'),
+            Image: $(element).find(imageSelector).attr('data-src')
         };
-        // console.log("Extracted client", clientObject);
         clients.push(clientObject);
     });
 
-    
-
-    // Check if there is a next page.
-    hasNextPage = $(".pagination-wrapper li[class='next'] a").length > 0;
-    page++;
+    hasNextPage = $(nextPageSelector).length > 0;
+    dynamicUrl = hasNextPage ? domain + $(nextPageSelector).attr("href") : false;
+    if (dynamicUrl) await delay(500);
 }
-// Save clients to Dataset - a table-like storage.
-console.log("Extracted clients", clients);
-await Actor.pushData({TotalItems:clients.length, Clients:clients});
 
-// Gracefully exit the Actor process. It's recommended to quit all Actors with an exit().
+const data = {TotalItems:clients.length, Clients:clients};
+console.log("Extracted data : ", data);
+await Actor.pushData(data);
+
 await Actor.exit();
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
